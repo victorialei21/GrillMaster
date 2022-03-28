e8c75948c7c0d8839862557b36b0f6d2ac550e80
victorialei21 <58520925+victorialei21@users.noreply.github.com>
2022-03-28 14:06:56 -0400

    background and patty images

--- core/src/main/kotlin/com/exampl/grillmaster/GrillMaster.kt
+++ core/src/main/kotlin/com/exampl/grillmaster/GrillMaster.kt
@@ -1,6 +1,44 @@
 package com.exampl.grillmaster
 
 import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.Color
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.BitmapFont
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
 
 /** [com.badlogic.gdx.ApplicationListener] implementation shared by all platforms. */
-class GrillMaster : ApplicationAdapter()
+class GrillMaster : ApplicationAdapter(){
+    lateinit var batch : SpriteBatch
+    lateinit var background : Texture
+    var width = 0f
+    var height = 0f
+    var w = 0f
+    var h = 0f
+    lateinit var font : BitmapFont
+    lateinit var patty: Array<Texture>
+    override fun create() {
+        super.create()
+        batch = SpriteBatch()
+        background = Texture("background.png")
+        width = Gdx.graphics.width.toFloat()
+        height = Gdx.graphics.height.toFloat()
+        w = 0.5f * width
+        h = 0.5f * height
+
+        patty = arrayOf(Texture("raw.png"), Texture("medium.png"),
+                        Texture("done.png"),Texture("burnt.png"))
+
+
+        font = BitmapFont()
+        font.color = Color.WHITE
+        font.data.setScale(10f)
+    }//create
+
+    override fun render() {
+        super.render()
+        batch.begin()
+        batch.draw(background, 0f, 0f, width, height)
+        batch.end()
+    }//render
+}//class GrillMaster
